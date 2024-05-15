4a59c09f25e542c5176e955f770d21c6ba033de1
AnhQuoc0912 <leanhquoc1611@gmail.com>
2024-05-15 20:06:42 +0700

    điều chỉnh theo csdl mới nhất

--- Controller/RequestController.js
+++ Controller/RequestController.js
@@ -1,8 +1,10 @@
-import * as RequestModel from './Model/RequestModel';
+import * as RequestModel from '../Model/RequestModel';
 
-export const addRequest = async (is_bookmarked_by_user, is_bookmarked_by_engineer, show_on_user, show_on_engineer) => {
+export const addRequest = async (is_bookmarked_by_user, is_bookmarked_by_engineer, show_on_user, show_on_engineer, 
+  vehicle_id, customer_id, engineer_id, longitude, latitude, status, notes, create_at) => {
     try {
-      await RequestModel.addRequest(is_bookmarked_by_user, is_bookmarked_by_engineer, show_on_user, show_on_engineer);
+      await RequestModel.addRequest(is_bookmarked_by_user, is_bookmarked_by_engineer, show_on_user, show_on_engineer, 
+        vehicle_id, customer_id, engineer_id, longitude, latitude, status, notes, create_at);
       console.log('Yêu cầu được thêm thành công');
     } catch (error) {
       console.error('Có lỗi khi thêm yêu cầu: ', error);
--- Model/RequestModel.js
+++ Model/RequestModel.js
@@ -1,15 +1,16 @@
-import { getDBConnection } from './database';
+import { getDBConnection } from '../database';
 
 export const addRequest =
-    async (is_bookmarked_by_user, is_bookmarked_by_engineer, show_on_user, show_on_engineer) => {
+    async (is_bookmarked_by_user, is_bookmarked_by_engineer, show_on_user, show_on_engineer, 
+        vehicle_id, customer_id, engineer_id, longitude, latitude, status, notes, create_at
+    ) => {
         const db = await getDBConnection();
         const query =
             `INSERT INTO Requests 
-        (is_bookmarked_by_user, 
-        is_bookmarked_by_engineer,
-        show_on_user, 
-        show_on_engineer) 
-        VALUES (?, ?, ?, ?)`;
+        (is_bookmarked_by_user, is_bookmarked_by_engineer, show_on_user, show_on_engineer, 
+            vehicle_id, customer_id, engineer_id, longitude, latitude, status, notes, create_at) 
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
         await db.executeSql(query,
-            [is_bookmarked_by_user, is_bookmarked_by_engineer, show_on_user, show_on_engineer]);
+            [is_bookmarked_by_user, is_bookmarked_by_engineer, show_on_user, show_on_engineer,
+                vehicle_id, customer_id, engineer_id, longitude, latitude, status, notes, create_at]);
     };
